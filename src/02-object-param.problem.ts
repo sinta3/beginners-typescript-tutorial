088b88f37ae73921dab5e487e44eaddcc29cd2b3
sinta3 <sinta.tri38@gmail.com>
2023-07-10 11:32:25 +0700

    fix(): exercise 1 and 2

--- src/02-object-param.problem.ts
+++ src/02-object-param.problem.ts
@@ -1,6 +1,11 @@
 import { expect, it } from "vitest";
 
-export const addTwoNumbers = (params) => {
+interface InputObj {
+  first: number,
+  second: number
+}
+
+export const addTwoNumbers = (params: InputObj) => {
   return params.first + params.second;
 };
 
